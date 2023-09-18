77013d329524cefe93e145111f4f331d0c650169
vladimirsuhanov <vladimir.sukhanov99@gmail.com>
2023-09-19 01:27:56 +0300

    added new transaction

--- src/features/newTransaction/NewTransaction.tsx
+++ src/features/newTransaction/NewTransaction.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { Typography } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+import { NewTransactionSearch } from './NewTransactionSearch';
+import { useQueryParams } from '../../shared/hooks/queryParams';
+import { NewTransactionAlert } from './NewTransactionAlert';
+import { useLazyUserInfoQuery, useUserInfoQuery } from '../../api/endpoints/user.endpoints';
 import { useLazyCreateTransactionQuery, useLazyUsersQuery } from '../../api/endpoints/transaction.endpoints';
 import { IUser } from '../../models/user';
-import { useLazyUserInfoQuery, useUserInfoQuery } from '../../api/endpoints/user.endpoints';
 
 export const NewTransaction = () => {
+	const queryParams = useQueryParams();
 
 	const [postUserRequest] = useLazyUsersQuery()
 	const [initialAmount, setInitialAmount] = useState(0);
@@ -16,6 +20,31 @@ export const NewTransaction = () => {
 	const [showSuccess, setShowSuccess] = useState(false);
 	const { data: userInfo } = useUserInfoQuery()
 
+	useEffect(() => {
+		const val = queryParams.get('amount');
+		if (val) {
+			setInitialAmount(parseFloat(val));
+			setAmount(parseFloat(val));
+		}
+
+	}, [queryParams.get('amount')])
+
+	useEffect(() => {
+		const val = queryParams.get('name');
+		if (val) {
+			postUserRequest({
+				filter: val
+			})
+				.unwrap()
+				.then((data: any[]) => {
+					if (data?.length) {
+						setSelectedUser(data[0])
+					}
+				})
+
+		}
+
+	}, [queryParams.get('name')])
 
 	const userChanged = (user: IUser | null) => {
 		setSelectedUser(user)
@@ -44,7 +73,7 @@ export const NewTransaction = () => {
 		<Typography sx={{marginBottom: '10px'}}>
 			Please select a user for the transaction
 		</Typography>
-
+		<NewTransactionSearch selectUser={userChanged}/>
 		{
 			selectedUser &&
             <div className="mt-10">
@@ -54,6 +83,12 @@ export const NewTransaction = () => {
                 <Typography sx={{marginBottom: '25px'}} variant="h5">
 					{selectedUser.name}
                 </Typography>
+                <NewTransactionAlert
+                    userInfo={userInfo}
+                    amount={amount}
+                    showSuccess={showSuccess}
+                    isError={isError}
+                ></NewTransactionAlert>
                 <LoadingButton
                     loading={isLoading}
                     disabled={amount < 1 || (userInfo?.balance ?? 0) < amount}
---
+++ src/features/newTransaction/NewTransactionSearch.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { Autocomplete, TextField } from '@mui/material';
+import { useLazyUsersQuery } from '../../api/endpoints/transaction.endpoints';
+import { IUser } from '../../models/user';
+import { useDebounce } from '../../shared/hooks/debounce';
+
+export interface INewTransactionSearchProps {
+	selectUser: (user: IUser | null) => void;
+	initialSearch?: string;
+}
+
+export const NewTransactionSearch = ({selectUser, initialSearch}: INewTransactionSearchProps) => {
+	const [postUserRequest, {data}] = useLazyUsersQuery()
+	const [search, setSearch] = useState(initialSearch ?? '');
+	const debounced = useDebounce(search);
+
+	useEffect(() => {
+		if (debounced) {
+			postUserRequest({
+				filter: debounced
+			})
+		}
+	}, [debounced])
+
+	const userChanged = (user: IUser | null) => {
+		selectUser(user);
+	}
+	return <Autocomplete
+		options={data && search ? data : []}
+		clearOnEscape
+		getOptionLabel={(option: IUser) => option.name}
+		onChange={(_e: React.SyntheticEvent, value: IUser | null) => userChanged(value)}
+		renderInput={(params) => (
+			<TextField
+				onChange={e => setSearch(e.target.value)} {...params} label="User" variant="standard" />
+		)}
+	/>
+}
