42ac25523418a52702ca8c9fac86fcb7fcaa6199
vladimirsuhanov <vladimir.sukhanov99@gmail.com>
2023-10-14 16:59:01 +0300

    feat(transactions list): added new data grid table with sorts, filters, pagination

--- src/features/transactionsList/TransactionList.tsx
+++ src/features/transactionsList/TransactionList.tsx
@@ -1,27 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import {
-	IconButton, InputAdornment, Pagination,
-	Paper,
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableHead, TablePagination,
-	TableRow, TextField
+	IconButton,
+	InputAdornment, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
+	TextField
 } from '@mui/material';
-import { Replay } from '@mui/icons-material';
 import { createSearchParams, useNavigate } from 'react-router-dom';
 import { useLazyGetTransactionQuery } from '../../api/endpoints/transaction.endpoints';
-import { ITransaction } from '../../models/transaction';
-import { Search, Clear } from '@mui/icons-material';
+import { ICreateTransactionTokenUser, ITransaction } from '../../models/transaction';
+import { Search, Clear, Replay } from '@mui/icons-material';
 import { useDebounce } from '../../shared/hooks/debounce';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { DataGridPro } from '@mui/x-data-grid-pro';
+import moment from 'moment';
 
 export const TransactionsList = () => {
 	const [search, setSearch] = useState('');
-	const [page, setPage] = useState(0);
-	const [rowsPerPage, setRowsPerPage] = useState(10);
-	const [paginationData, setPaginationData] = useState<ITransaction[]>([]);
-	const [getTransactionsRequest, {data}] = useLazyGetTransactionQuery()
+	const [getTransactionsRequest, { data }] = useLazyGetTransactionQuery()
 	const debounced = useDebounce(search);
 	useEffect(() => {
 		getTransactionsRequest();
@@ -38,93 +32,48 @@ export const TransactionsList = () => {
 		})
 	}
 
-	const handleChangePage = (
-		event: React.MouseEvent<HTMLButtonElement> | null,
-		newPage: number,
-	) => {
-		setPage(newPage);
-	};
-
-	const handleChangeRowsPerPage = (
-		event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-	) => {
-		setRowsPerPage(parseInt(event.target.value, 10));
-		setPage(0);
-	};
-
-	useEffect(() => {
-		const startIndex = page * rowsPerPage
-		setPaginationData(data?.slice(startIndex, startIndex + rowsPerPage) ?? [])
-	}, [page, data]);
-
-	useEffect(() => {
-		setPage(0)
-	}, [rowsPerPage, data]);
+	const columns: GridColDef[] = [
+		{
+			field: 'created', headerName: 'Date/Time', width: 200,
+			valueGetter: (params: GridValueGetterParams<ITransaction>) => `${moment(params.row.created, moment.ISO_8601).local().fromNow()}`,
+		},
+		{
+			field: 'recipientUser',
+			headerName: 'Username',
+			flex: 1,
+			valueGetter: (params: GridValueGetterParams<ITransaction>) => `${params.row.recipientUser.name}`,
+		},
+		{
+			field: 'amount',
+			headerName: 'Amount',
+			type: 'number',
+		},
+		{
+			field: 'actions',
+			headerName: 'Repeat',
+			type: 'actions',
+			width: 100,
+			getActions: (params) => {
+				return [<div>
+					<IconButton onClick={() => handleRepeat(params.row.recipientUser.name, params.row.amount)}>
+						<Replay/>
+					</IconButton>
+				</div>]
+			}
+		},
+	];
 
 	return <>
-		<div className="mb-3 flex justify-between">
-			<TextField
-				InputProps={{
-					startAdornment: (
-						<InputAdornment position="start">
-							<Search />
-						</InputAdornment>
-					),
-					endAdornment: (
-						<InputAdornment
-							position="end"
-							style={{ display: search ? undefined : 'none', cursor: 'pointer' }}
-							onClick={() => setSearch('')}
-						>
-							<Clear />
-						</InputAdornment>
-					)
-				}}
-				value={search}
-				onChange={e => setSearch(e.target.value)}
-				className="w-80" id="trasaction-search" label="Username" variant="standard" />
-			<TablePagination
-				component="div"
-				count={data?.length ?? 0}
-				page={page}
-				onPageChange={handleChangePage}
-				rowsPerPage={rowsPerPage}
-				onRowsPerPageChange={handleChangeRowsPerPage}
-			/>
-		</div>
-
-		<TableContainer component={Paper}>
-			<Table>
-				<TableHead>
-					<TableRow>
-						<TableCell>Date/Time</TableCell>
-						<TableCell>Username</TableCell>
-						<TableCell align="right">Amount</TableCell>
-						<TableCell align="center">Repeat</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{paginationData.map((row) => (
-						<TableRow
-							key={row.id}
-							sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-						>
-							<TableCell component="th" scope="row">
-								{row.created}
-							</TableCell>
-							<TableCell component="th" scope="row">
-								{row.recipientUser.name}
-							</TableCell>
-							<TableCell align="right">{row.amount}</TableCell>
-							<TableCell align="center">
-								<IconButton onClick={() => handleRepeat(row.recipientUser.name, row.amount)}>
-									<Replay/>
-								</IconButton>
-							</TableCell>
-						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
+		<DataGrid
+			rows={data ?? []}
+			columns={columns}
+			initialState={{
+				pagination: {
+					paginationModel: { page: 0, pageSize: 5 },
+				},
+			}}
+			pageSizeOptions={[5, 10]}
+			checkboxSelection
+		/>
 	</>
 }
